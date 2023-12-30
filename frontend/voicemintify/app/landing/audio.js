4a6765c867ee922207a1a4354f304cfd29e7e497
MagneticGlitter <92243872+MagneticGlitter@users.noreply.github.com>
2023-12-29 23:37:44 -0500

    Update audio.js

--- frontend/voicemintify/app/landing/audio.js
+++ frontend/voicemintify/app/landing/audio.js
@@ -7,3 +7,20 @@ if ("webkitSpeechRecognition" in window) {
   recognition.lang = "en-US";
 }
 
+const useSpeechRecognition = () => {
+    const [text, setText] = useState("");
+    const [isListening, setIsListening] = useState(false);
+
+    useEffect(() => {
+        if (!recognition) {
+            return;
+        }
+
+        recognition.onresult = (event) => {
+            console.log("onresult event: ", event);
+            setText(event.results[0][0].transcript);
+            recognition.stop();
+            setIsListening(false);
+        };
+    }, []);
+
